7a85c33f13c812b0b1c1e9bb86047595441df4c0
Jhordy Rosario <jhordyrosario@gmail.com>
2024-06-29 22:07:19 -0400

    - Adding a new and better solution for TwoSum Algorithm

--- c-sharp/AlgorithmsCodingChallenges/AlgorithmsCodingChallenges.UnitTests/TwoSumSolutionsTests.cs
+++ c-sharp/AlgorithmsCodingChallenges/AlgorithmsCodingChallenges.UnitTests/TwoSumSolutionsTests.cs
@@ -26,8 +26,17 @@ public class Tests
     [TestCase(new int[] {2,7,11,15}, 9, new int[] {0,1})]
     [TestCase(new int[] {3,2,4}, 6, new int[] {1,2})]
     [TestCase(new int[] {3,3}, 6, new int[] {0,1})]
-    public void TwoSu2_WhenCalled_ShouldReturnCorrectIndexes(int[] nums, int target, int[] response)
+    public void TwoSum2_WhenCalled_ShouldReturnCorrectIndexes(int[] nums, int target, int[] response)
     {
         Assert.That(_twoSumSolutions.TwoSum2(nums, target), Is.EqualTo(response));
+    }
+
+    [Test]
+    [TestCase(new int[] {2,7,11,15}, 9, new int[] {0,1})]
+    [TestCase(new int[] {3,2,4}, 6, new int[] {1,2})]
+    [TestCase(new int[] {3,3}, 6, new int[] {0,1})]
+    public void TwoSum3_WhenCalled_ShouldReturnCorrectIndexes(int[] nums, int target, int[] response)
+    {
+        Assert.That(_twoSumSolutions.TwoSum3(nums, target), Is.EqualTo(response));
     }
 }
--- c-sharp/AlgorithmsCodingChallenges/AlgorithmsCodingChallenges/TwoSumSolutions.cs
+++ c-sharp/AlgorithmsCodingChallenges/AlgorithmsCodingChallenges/TwoSumSolutions.cs
@@ -3,7 +3,6 @@ namespace AlgorithmsCodingChallenges;
 public class TwoSumSolutions
 {
     public int[] TwoSum1(int[] nums, int target) {
-        
         for(var i=0; i<=nums.Length-2 ; i++)
         {
             for(var j=i+1; j<=nums.Length-1; j++)
@@ -24,14 +23,39 @@ public class TwoSumSolutions
 
         for(var i=0; i<nums.Length ; i++)
         {
+            valuesIndexes[nums[i]] = i;
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int searchedValue = target - nums[i];
+
+            if (valuesIndexes.ContainsKey(searchedValue) && valuesIndexes[searchedValue] != i)
+            {
+                return [i, valuesIndexes[searchedValue]];
+            }
+        }
+
+        throw new Exception("Two Sum Not Found");
+    }
+
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+
+     public int[] TwoSum3(int[] nums, int target) {
+        
+        var valuesIndexes = new Dictionary<int, int>();
+
+        for(var i=0; i<nums.Length ; i++)
+        {
             var searchedValue = target - nums[i];
 
-            if(valuesIndexes.TryGetValue(searchedValue, out var searchedIndex))
+            if(valuesIndexes.ContainsKey(searchedValue))
             {
-                return [searchedIndex, i];
+                return [valuesIndexes[searchedValue], i];
             }
 
-            valuesIndexes.Add(nums[i], i);
+            valuesIndexes[nums[i]] =  i;
         }
 
         throw new Exception("Two Sum Not Found");
