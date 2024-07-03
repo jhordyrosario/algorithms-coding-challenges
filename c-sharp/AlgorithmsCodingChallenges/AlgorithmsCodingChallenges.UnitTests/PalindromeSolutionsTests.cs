af2e56bbdbeff44f8d56d0e1f31beff64e91ed5c
Jhordy Rosario <jhordyrosario@gmail.com>
2024-07-03 00:08:39 -0400

    Adding test cases for palindrome solutions

---
+++ c-sharp/AlgorithmsCodingChallenges/AlgorithmsCodingChallenges.UnitTests/PalindromeSolutionsTests.cs
@@ -0,0 +1,27 @@
+namespace AlgorithmsCodingChallenges.UnitTests;
+using AlgorithmsCodingChallenges;
+
+[TestFixture]
+public class PalindromeSolutionsTests
+{
+    private PalindromeSolutions _palindromeSolutions { get; set; }
+
+    [SetUp]
+    public void SetUp()
+    {
+        _palindromeSolutions = new PalindromeSolutions();
+    }
+
+    [Test]
+    [TestCase(1, true)]
+    [TestCase(12, false)]
+    [TestCase(131, true)]
+    [TestCase(2442, true)]
+    [TestCase(355553, true)]
+    [TestCase(624232426, true)]
+    [TestCase(13134, false)]
+    public void IsPalindrome_WhenCalled_ShouldReturnTrueForPalindromeNumbers(int number, bool isPalindrome)
+    {
+        Assert.That(_palindromeSolutions.IsPalindrome(number), Is.EqualTo(isPalindrome));
+    }  
+}
